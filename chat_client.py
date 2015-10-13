a958e7adbe872fa04d3390acc724b0b8ff1761f9
atsavin <atsavin.halliem@gmail.com>
2015-10-13 13:27:58 +0700

    update
    adding login, private, list feature

--- chat_client.py
+++ chat_client.py
@@ -3,19 +3,40 @@ import socket
 import select
  
 def chat_client():
+#    if(len(sys.argv) < 3) :
+#        print 'Usage : python chat_client.py hostname port'
+#        sys.exit()
+
+#    host = sys.argv[1]
+#    port = int(sys.argv[2])
     
     # Create a TCP/IP socket
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_sock.settimeout(2)
      
     # Connect the socket to the port where the server is listening
+    
+    while True:
+	    print "Terkoneksi dengan host\n"
+	    print "\nMenu Command:\n 1. send = untuk private message\n 2. sendall = untuk broadcast\n 3. liston = untuk list yang online\n 4. login = untuk login pertama kali dikuti nama\n"
+	    inp = sys.stdin.readline()
+	    inp1 = inp.split(' ', 1)[0]
+
+	    if inp1=="login":
+		print("Logged on, start chatting");
+		break
+	    else : 
+		print "You Must Login"
+    
     try :
         client_sock.connect(('localhost', 10000))
     except :
         print 'Gagal Konek'
         sys.exit()
-     
-    print 'Terkoneksi ke Host'
+
+    inp2 = inp.split(' ',1)[1]
+    client_sock.sendall(inp)
+
     sys.stdout.write('[Saya] '); sys.stdout.flush()
      
     while True:
@@ -31,7 +52,10 @@ def chat_client():
                     sys.exit()
                 else :
                     sys.stdout.write(data)
-                    sys.stdout.write('[Saya] '); sys.stdout.flush()     
+		    if data == "\nNama sudah dipakai\n" :
+			sys.exit()
+		    else :
+                        sys.stdout.write('[Saya] '); sys.stdout.flush()     
             
             else :
                 msg = sys.stdin.readline()
